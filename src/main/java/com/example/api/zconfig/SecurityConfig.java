1df33f2233a7925526e6aa44a93b185bf44733e1
ryeol22e <ryeol22e@gmail.com>
2022-08-20 16:31:07 +0900

    소스 정리

--- src/main/java/com/example/api/zconfig/SecurityConfig.java
+++ src/main/java/com/example/api/zconfig/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.example.api.zconfig;
 
-import java.net.http.HttpHeaders;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
