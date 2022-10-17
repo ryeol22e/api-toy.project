2477f85f2b741beeba11a08a8d3099f2cc85a106
이재렬 <jaeryeol@3top.co.kr>
2022-10-17 09:32:22 +0900

    redis 임시삭제

--- src/main/java/com/example/api/zconfig/RedisConfig.java
+++ src/main/java/com/example/api/zconfig/RedisConfig.java
@@ -17,14 +17,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-	@Value("${spring.redis.host}")
-	private String host;
-	@Value("${spring.redis.port}")
-	private int port;
-
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(host, port);
+		return new LettuceConnectionFactory();
 	}
 
 	@Bean
